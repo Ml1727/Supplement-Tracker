f64d674f6ed3c6c9fc66d172932eeb779983ff1c
Ml1727 <gsishir2004@gmail.com>
2025-05-19 16:51:30 +0000

    Initial commit of supplement tracker app

---
+++ src/main/java/com/example/supplement/SupplementReminderApp.java
@@ -0,0 +1,11 @@
+package com.example.supplement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplementReminderApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SupplementReminderApp.class, args);
+    }
+}
